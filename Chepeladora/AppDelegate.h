12c9d0b13aeac50e06bb4c7c62c2e8ac25478fa6
J.Carlos C.S <WeRiChCh@macbook-de-jose.gateway.2wire.net>
2012-05-28 19:06:12 -0500

    Initial Commit

---
+++ Chepeladora/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Chepeladora
+//
+//  Created by José Carlos Cortés Soto on 28/05/12.
+//  Copyright (c) 2012 Universidad de Guanajuato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Chepeladora/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Chepeladora
+//
+//  Created by José Carlos Cortés Soto on 28/05/12.
+//  Copyright (c) 2012 Universidad de Guanajuato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
